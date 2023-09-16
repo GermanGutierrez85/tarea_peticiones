c56712ea9ec440161e3a2626d1b22803426ee9d8
GermanGutierrez85 <guticen12@gmail.com>
2023-09-16 16:33:02 -0300

    arreglos

--- src/components/CreateUserForm.jsx
+++ src/components/CreateUserForm.jsx
@@ -4,32 +4,42 @@ import axios from "axios";
 export default function CreateUserForm() {
   const [state, setState] = useState("");
   function handleSubmit(e) {
+    e.preventDefault();
+    let name = e.target.name.value;
+    let job = e.target.job.value;
     const userForm = {
-      name: e.target.name.value,
-      job: e.target.job.value,
+      name: name,
+      job: job,
     };
     console.log(userForm);
+    if (name == "" || job == "") {
+      setState("Debe completar los espacios");
+    } else {
+      axios
+        .post("https://reqres.in/api/users", userForm)
+        .then((response) => {
+          setState("Usuario creado con exito");
+          console.log(response.status);
+          response.status = "201"
+            ? setState("Usuario creado con exito")
+            : setState("Debes completar los datos requeridos");
+        })
 
-    axios
-      .post("https://reqres.in/api/users", userForm)
-      .then((response) => {
-        setState("Usuario creado con exito");
-        console.log(response.status);
-        response.status = OK
-          ? setState("Usuario creado con exito")
-          : setState("Debes completar los datos requeridos");
-      })
-
-      .catch((error) => console.log(setState(error)));
+        .catch((error) => {
+          setState("Error al crear usuario - Intente nuevamente");
+          console.log(error);
+        });
+    }
   }
   function handleReset(e) {
     e.target.name.value = "";
     e.target.job.value = "";
+    setState("");
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="container">
         <label>
           Name:
           <input type="text" name="name" />
@@ -40,9 +50,10 @@ export default function CreateUserForm() {
           <input type="text" name="job" />
         </label>
         <input type="submit" value="Crear usuario" />
+        <input type="reset" value="Reset" onClick={() => handleReset} />
       </form>
-      {/* <input type="reset" value="Reset" onClick={() => handleReset} /> */}
-      <p style={{ color: "black" }}>{state}</p>
+
+      <p style={{ color: "white" }}>{state}</p>
     </>
   );
 }
